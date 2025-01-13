2cafc7fca39f9d8abdff83e0a9e2aa8be2066dcd
vkalraa <65603096+vkalraa@users.noreply.github.com>
2025-01-14 00:31:47 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,19 +1,19 @@
 info = {
    "Pronoun": "her", 
-   "Name": "Vicky",
-   "Full_Name": "Vicky Kuo",
-   "Intro": "A Tech Educator and AI Enthusiast at cognitiveclass.ai",
-   "About":"Hey there, I'm Vicky! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. Check out my free courses and projects on cognitiveclass.ai: https://cognitiveclass.ai/",
-   "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
-   "Medium":"https://medium.com/@vicky-note/about",
-   "City":"Toronto, Canada",
-   "Photo":"""<a href=\"https://www.linkedin.com/in/vicky-tck/\"><img src=\"https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/profile.png" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
-   "Email": "vicky.kuo.contact@gmail.com"
+   "Name": "Vani",
+   "Full_Name": "Vani Kalra",
+   "Intro": "A Data Analyst and AI Enthusiast passionate about business and technology.",
+   "About": "Hi there, I'm Vani! I'm driven by the power of data to uncover insights and create impactful solutions. Check out my projects and portfolio to see how I leverage data and technology for business success.",
+   "Tableau": "https://public.tableau.com/app/profile/vani.kalra/viz/PortfolioOverview/Overview",
+   "Medium": "https://medium.com/@kalra.vani18",
+   "City": "Madison, Wisconsin, USA",
+   "Photo": """<a href=\"https://www.linkedin.com/in/vani-kalra/\"><img src=\"https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/profile.png" width=\"200\" alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "kalra.vani18@gmail.com"
 }
 
-embed_rss= {
-    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
-        data-rss="https://medium.com/feed/@vicky-note"
+embed_rss = {
+    'rss': """<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@kalra.vani18"
         data-maxcols="3" 
         data-layout="grid"
         data-poststyle="inline" 
@@ -27,3 +27,4 @@ endorsements = {
     "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
     "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
 }
+
